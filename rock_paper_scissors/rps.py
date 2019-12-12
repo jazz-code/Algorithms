ee56a5a7c6a6d39d6f94fae0d665f84959554d70
ren.meta@gmail.com <ren.meta@gmail.com>
2019-12-12 17:46:43 -0500

    rock paper scissors algoritm finished- tests passing

--- rock_paper_scissors/rps.py
+++ rock_paper_scissors/rps.py
@@ -3,7 +3,26 @@
 import sys
 
 def rock_paper_scissors(n):
-  pass 
+    outcomes = []
+    plays = ['rock', 'paper', 'scissors']
+
+    if n == 0:
+        return [[]]
+
+    for play in plays:
+       def find_outcome(rounds_left, result):
+            if rounds_left == 0:
+                outcomes.append(result)
+                return
+
+            for play in plays:
+                find_outcome(rounds_left - 1, result + [play])
+
+    find_outcome(n, [])
+
+    return outcomes
+
+# print(rock_paper_scissors(2))
 
 
 if __name__ == "__main__":
